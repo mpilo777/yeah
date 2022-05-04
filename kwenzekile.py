18e52aed1e9ed8fb155f50bdeab418c3380b119b
mpilo777 <104911631+mpilo777@users.noreply.github.com>
2022-05-04 16:01:55 +0200

    Add files via upload
    aybo aybo

---
+++ kwenzekile.py
@@ -0,0 +1,59 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+1+1
+2
+print(Hello, World)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print(Hello, World)
+NameError: name 'Hello' is not defined
+print("Hello,World")
+Hello,World
+
+=========== RESTART: C:/Users/Mpilo/Documents/STATS707/Hello World.py ==========
+Hello, World
+print("Hello,World)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+
+=========== RESTART: C:/Users/Mpilo/Documents/STATS707/Hello World.py ==========
+Traceback (most recent call last):
+  File "C:/Users/Mpilo/Documents/STATS707/Hello World.py", line 1, in <module>
+    print(Hello, World)
+NameError: name 'Hello' is not defined
+777 = "The Promised Land"
+      
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+Haven = "The Promised Land"
+      
+print(Haven)
+      
+The Promised Land
+_777 = "777 Haven"
+      
+print(_777)
+      
+777 Haven
+_777
+      
+'777 Haven'
+Haven
+      
+'The Promised Land'
+x = 7
+      
+y = '7'
+      
+print(x)
+      
+7
+print(y)
+      
+7
+x
+      
+7
+y
+      
+'7'
+man = input("2")
